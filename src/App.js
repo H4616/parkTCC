aff4608831b4cccba0b388436b7a9911d5652099
H4616 <103203538+H4616@users.noreply.github.com>
2025-03-04 00:19:23 +0700

    frontend note finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter ,Routes, Route} from "react-router-dom";
 import NoteCardList from "./components/note_card_list.js";
-import Add_note from "./components/Add_note.js";
+import AddNote from "./components/Add_note.js";
 import EditNote from "./components/edit_note.js";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<NoteCardList />} />
-          <Route path="/add_note" element={<Add_note />} />
+          <Route path="/add_note" element={<AddNote />} />
           <Route path="/edit_note/:id" element={<EditNote />} />
         </Routes>
       </BrowserRouter>
